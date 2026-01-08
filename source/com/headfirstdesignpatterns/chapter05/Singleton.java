e486e16fd67acb1f8cd6ff8dc059ad8dad31b73d
maksimmerkulov <merkulov.mv@outlook.com>
2026-01-08 21:57:33 +0500

    Chapter 5: Dealing with multithreading (One of a Kind Objects)

--- source/com/headfirstdesignpatterns/chapter05/Singleton.java
+++ source/com/headfirstdesignpatterns/chapter05/Singleton.java
@@ -1,18 +1,11 @@
 package com.headfirstdesignpatterns.chapter05;
 
 public class Singleton {
-    private static Singleton uniqueInstance;
-
-    // other useful instance variables here
+    private static Singleton uniqueInstance = new Singleton();
 
     private Singleton() {}
 
-    public static synchronized Singleton getInstance() {
-        if (uniqueInstance == null) {
-            uniqueInstance = new Singleton();
-        }
+    public static Singleton getInstance() {
         return uniqueInstance;
     }
-
-    // other useful methods here
 }
